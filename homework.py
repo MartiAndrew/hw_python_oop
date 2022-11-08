90209a9c573fd031676e115ff611eac10b421407
MartiAndrew <andrew.mishkov@gmail.com>
2022-11-09 00:15:19 +0300

    final changes2

--- homework.py
+++ homework.py
@@ -98,7 +98,7 @@ class SportsWalking(Training):
         result = (self.CALORIES_WEIGHT_MULTIPLIER * self.weight
                   + (mean_speed_mc ** 2 / (self.height / self.CM_IN_M))
                   * self.CALORIES_SPEED_HEIGHT_MULTIPLIER * self.weight) \
-                  * self.duration * self.MIN_IN_H
+                 * self.duration * self.MIN_IN_H
         return result
 
     def show_training_info(self) -> InfoMessage:
@@ -111,32 +111,31 @@ class Swimming(Training):
     CALORIES_MEAN_SPEED_SHIFT = 1.1
     CALORIES_WEIGHT_MULTIPLIER = 2
 
-
     def __init__(self, action: int,
                  duration: float,
                  weight: float,
-                length_pool: float,
+                 length_pool: float,
                  count_pool: float) -> None:
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-         return (self.length_pool * self.count_pool / self.M_IN_KM
+        return (self.length_pool * self.count_pool / self.M_IN_KM
                  / self.duration)
 
     def get_distance(self) -> float:
-         return super().get_distance()
+        return super().get_distance()
 
     def get_spent_calories(self) -> float:
         return (
-            (self.get_mean_speed()
-            + self.CALORIES_MEAN_SPEED_SHIFT)
-            * self.CALORIES_WEIGHT_MULTIPLIER * self.weight
-            * self.duration
+                (self.get_mean_speed()
+                 + self.CALORIES_MEAN_SPEED_SHIFT)
+                * self.CALORIES_WEIGHT_MULTIPLIER * self.weight
+                * self.duration
         )
 
-    def show_training_info(self)-> InfoMessage:
+    def show_training_info(self) -> InfoMessage:
         return super().show_training_info()
 
 
